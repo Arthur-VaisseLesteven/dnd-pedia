0c761ace80fdf1ccc12c596b040dbf537fe5229b
Arthur Vaisse-Lesteven <arthurvaisse@yahoo.fr>
2026-03-22 14:07:58 +0100

    Refactor index page by introducing menu-entry component

---
+++ site/js/components/menu-entry.js
@@ -0,0 +1,26 @@
+class MenuEntry extends HTMLElement {
+
+    constructor() {
+        super();
+
+        let href = this.getAttribute('href');
+        let src = this.getAttribute('src');
+        let title = this.getAttribute('title');
+        let subtitle = this.getAttribute('subtitle');
+
+        this.innerHTML = `
+            <nav class="category-menu">
+                <a href="${href}" class="category-card">
+                    <img class="category-card__image" src="${src}" alt="=${title}">
+                    <div class="category-card__body">
+                        <div class="category-card__title">${title}</div>
+                        <div class="category-card__subtitle">${subtitle}</div>
+                    </div>
+                </a>
+            </nav>
+        `;
+    }
+
+}
+
+customElements.define('menu-entry', MenuEntry);
