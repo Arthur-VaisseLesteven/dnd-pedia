53aab43080c20958ddb158170cf46152961b84d0
arthurvaisse-lesteven <arthurvaisse@yahoo.fr>
2026-03-29 22:57:36 +0200

    improve main menu style

--- site/js/components/menu-entry.js
+++ site/js/components/menu-entry.js
@@ -9,15 +9,13 @@ class MenuEntry extends HTMLElement {
         let subtitle = this.getAttribute('subtitle');
 
         this.innerHTML = `
-            <nav class="category-menu">
-                <a href="${href}" class="category-card">
-                    <img class="category-card__image" src="${src}" alt="=${title}">
-                    <div class="category-card__body">
-                        <div class="category-card__title">${title}</div>
-                        <div class="category-card__subtitle">${subtitle}</div>
-                    </div>
-                </a>
-            </nav>
+            <a href="${href}" class="category-card">
+                <img class="category-card__image" src="${src}" alt="=${title}">
+                <div class="category-card__body">
+                    <div class="category-card__title">${title}</div>
+                    <div class="category-card__subtitle">${subtitle}</div>
+                </div>
+            </a>
         `;
     }
 
